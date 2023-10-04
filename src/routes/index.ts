9f565980c9c4dfbf673b056d4eadf8c051f9b4d2
João  Víctor Antunes <joao.extremo722@gmail.com>
2023-10-03 21:48:46 -0300

    feat Agenda

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,13 +4,14 @@ import HomeRouter from './Home';
 import PacienteRouter from './Paciente';
 import { consultaRouter } from './Consulta';
 import secretariasRouter from './Secretaria';
+import { agendaRouter } from './Agenda';
 
 const router = express.Router();
 
 router.use("/", HomeRouter)
 router.use('/paciente', PacienteRouter);
 router.use('/consultas', consultaRouter);
-// router.use('/agendas', agendasRouter);
+router.use('/agendas', agendaRouter);
 router.use('/secretarias', secretariasRouter);
 
 export default router
